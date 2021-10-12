aed809d15b3ae518df811dfa05738891284196d9
imdad Adelabou <imdadadelabou0@gmail.com>
2021-10-12 14:34:07 +0100

    currently custom exchange page

---
+++ web/views/ExchangeView.js
@@ -0,0 +1,13 @@
+app.component('exchange-view', {
+    template:
+    /*html*/
+        `
+    <div id="exchangeView">
+        <label>Entrez le montant en FCFA/XOF</label>
+        <input type="number">
+        <label>Le montant entrez équivaut à </label>
+        <input type="number">
+        <button type="button">Validez l'achat</button>
+    </div>
+    `
+})
